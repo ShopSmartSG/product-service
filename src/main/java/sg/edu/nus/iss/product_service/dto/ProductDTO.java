c7bc9941603bd1af054c332ff8f90eb8d6966683
Anvisimi <8444.sa@gmail.com>
2024-10-03 13:57:03 +0800

    Merchant endpoints

--- src/main/java/sg/edu/nus/iss/product_service/dto/ProductDTO.java
+++ src/main/java/sg/edu/nus/iss/product_service/dto/ProductDTO.java
@@ -3,6 +3,7 @@ package sg.edu.nus.iss.product_service.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.NonNull;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +22,11 @@ public class ProductDTO {
     @NotBlank(message = "Please provide an image URL")
     private String imageUrl;
     private String productDescription;
-    @NonNull
+    @Positive(message = "Please provide a valid original price")
     private BigDecimal originalPrice;
-    @NonNull
+    @Positive(message = "Please provide a valid listing price")
     private BigDecimal listingPrice;
-    @NonNull
+    @Positive(message = "Please provide a valid available stock")
     private int availableStock;
     @NonNull
     private UUID merchantId;
