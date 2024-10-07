e6b5903baca390cb944fe56ed7347e37d97dab59
Anvisimi <8444.sa@gmail.com>
2024-10-07 13:55:07 +0800

    test fixes in main

--- src/test/java/sg/edu/nus/iss/product_service/ProductServiceApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/product_service/ProductServiceApplicationTests.java
@@ -1,13 +1,7 @@
 package sg.edu.nus.iss.product_service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
