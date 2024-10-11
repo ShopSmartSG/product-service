198bb1364431019bb0dfcf65f7507904a3720932
Anvisimi <8444.sa@gmail.com>
2024-10-11 17:17:02 +0800

    removed product list

--- src/main/java/sg/edu/nus/iss/product_service/dto/CategoryDTO.java
+++ src/main/java/sg/edu/nus/iss/product_service/dto/CategoryDTO.java
@@ -12,4 +12,5 @@ public class CategoryDTO {
     private UUID categoryId;
     @NotBlank(message = "Category name is mandatory")
     private String categoryName;
+    private String categoryDescription;
 }
