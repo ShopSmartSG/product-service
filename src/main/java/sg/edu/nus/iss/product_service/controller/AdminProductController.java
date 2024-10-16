521ec7ef6e8a3c979ac384f67977d0e7f5d605e0
Krishna Rishi Senthil Kumar <e1353095@u.nus.edu>
2024-10-16 14:47:45 +0800

    Basic Admin Curd

---
+++ src/main/java/sg/edu/nus/iss/product_service/controller/AdminProductController.java
@@ -0,0 +1,140 @@
+package sg.edu.nus.iss.product_service.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import sg.edu.nus.iss.product_service.dto.ProductDTO;
+import sg.edu.nus.iss.product_service.exception.ResourceNotFoundException;
+import sg.edu.nus.iss.product_service.model.Category;
+import sg.edu.nus.iss.product_service.model.Product;
+import sg.edu.nus.iss.product_service.service.CategoryService;
+import sg.edu.nus.iss.product_service.service.ProductService;
+import sg.edu.nus.iss.product_service.utility.S3Utility;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin")
+@Tag(name = "Admin Product API", description = "APIs for admin to create, read, update, and delete products")
+public class AdminProductController {
+    private final ProductService productService;
+    private final S3Utility s3Service;
+    private final ObjectMapper objectMapper;
+    private final CategoryService categoryService;
+
+    @Autowired
+    public AdminProductController(ProductService productService, ObjectMapper objectMapper, S3Utility s3Service, CategoryService categoryService) {
+        this.productService = productService;
+        this.objectMapper = objectMapper;
+        this.s3Service = s3Service;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/products")
+    @Operation(summary = "Retrieve all products")
+    public ResponseEntity<?> getAllProducts(@RequestParam(required = false) UUID merchantId, @RequestParam(required = false) Integer page, @RequestParam(required = false) Integer size) {
+        if (page != null && size != null) {
+            Pageable pageable = PageRequest.of(page, size);
+            Page<Product> products = productService.getAllProducts(merchantId, pageable);
+            return ResponseEntity.ok(products);
+        } else {
+            List<Product> products = productService.getProductsByMerchantId(merchantId);
+            return ResponseEntity.ok(products);
+        }
+    }
+
+    @GetMapping("/products/{productId}")
+    @Operation(summary = "Retrieve product By ID")
+    public ResponseEntity<?> getProductById(@RequestParam(required = false) UUID merchantId, @PathVariable UUID productId) {
+        Product product = productService.getProductByIdAndMerchantId(merchantId, productId);
+        if (product == null) {
+            throw new ResourceNotFoundException("Product not found");
+        }
+        return ResponseEntity.ok(product);
+    }
+
+
+    @GetMapping("/{merchantId}/categories/{categoryId}")
+    @Operation(summary = "Retrieve products by merchant ID and category ID")
+    public ResponseEntity<?> getProductByMerchantIdAndCategoryId(@PathVariable UUID merchantId, @PathVariable UUID categoryId, @RequestParam(required = false) Integer page, @RequestParam(required = false) Integer size) {
+        if (page != null && size != null) {
+            Pageable pageable = PageRequest.of(page, size);
+            Page<Product> products = productService.getProductsByMerchantIdAndCategoryId(merchantId, categoryId, pageable);
+            return ResponseEntity.ok(products);
+        } else {
+            List<Product> products = productService.getProductsByMerchantIdAndCategoryId(merchantId, categoryId);
+            return ResponseEntity.ok(products);
+        }
+    }
+
+    @PostMapping("/products")
+    @Operation(summary = "Add a new product")
+    public ResponseEntity<?> addProduct( @Valid @RequestBody Product product) {
+        // check if category exists
+        // if it doesn't ask merchant to create category first
+        Category category = categoryService.getCategoryByName(product.getCategory().getCategoryName());
+        if (category == null) {
+            throw new ResourceNotFoundException("Category not found , Please create category first");
+        }
+        product.setCategory(category);
+        Product newProduct = productService.addProduct(product);
+        return ResponseEntity.ok(newProduct);
+    }
+
+    @PostMapping("/images/upload")
+    @Operation(summary = "Upload a product image", description = "Uploads a product image to S3")
+    public ResponseEntity<String> uploadImage(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "File to upload",
+                    required = true,
+                    content = @Content(mediaType = "multipart/form-data",
+                            schema = @Schema(type = "string", format = "binary"))
+            )
+            @RequestPart("file") MultipartFile file
+    ) throws IOException {
+        String fileName = s3Service.uploadFile(file);
+        String fileUrl = s3Service.getFileUrl(fileName);
+        return ResponseEntity.ok(fileUrl);
+    }
+
+    @DeleteMapping("/products/{productId}")
+    @Operation(summary = "Delete product by Product ID")
+    public ResponseEntity<String> deleteProduct(@RequestParam (required = false) UUID merchantId, @PathVariable UUID productId) {
+        Product existingProduct = productService.getProductByIdAndMerchantId(merchantId, productId);
+        if (existingProduct == null) {
+            throw new ResourceNotFoundException("Product not found");
+        }
+        productService.deleteProduct(existingProduct);
+        return ResponseEntity.ok("Delete: successful");
+    }
+
+    @PutMapping("/{merchantId}/products/{productId}")
+    @Operation(summary = "Update product")
+    public ResponseEntity<?> updateProduct(@PathVariable UUID merchantId, @PathVariable UUID productId, @Valid @RequestBody ProductDTO dto) {
+        Product existingProduct = productService.getProductByIdAndMerchantId(merchantId, productId);
+        if (existingProduct == null) {
+            throw new ResourceNotFoundException("Product not found");
+        }
+
+        if (!existingProduct.getProductId().equals(dto.getProductId())) {
+            throw new IllegalArgumentException("Product ID mismatch");
+        }
+        existingProduct = objectMapper.convertValue(dto, Product.class);
+        existingProduct.setProductId(productId);
+        existingProduct.setCategory(categoryService.getCategoryById(dto.getCategoryId()));
+        return ResponseEntity.ok(productService.updateProduct(existingProduct));
+    }
+}
