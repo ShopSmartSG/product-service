88a66f76c1d6c811cce20e3e0622c4b854a7e66e
Anvisimi <8444.sa@gmail.com>
2025-03-05 23:37:08 +0800

    SAST and DAST fixes V7

--- src/main/java/sg/edu/nus/iss/product_service/ProductServiceApplication.java
+++ src/main/java/sg/edu/nus/iss/product_service/ProductServiceApplication.java
@@ -2,11 +2,12 @@ package sg.edu.nus.iss.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @Configuration
 public class ProductServiceApplication {
 
